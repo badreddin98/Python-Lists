2bb4311004d98c24eaa27a72c3b5db5a6d09d660
badreddin98 <56277339+badreddin98@users.noreply.github.com>
2024-07-14 12:26:56 -0400

    first commit

---
+++ advancedListMethodsAndIdentityOperators.py
@@ -0,0 +1,7 @@
+# Task 1: Given the two lists:
+submitted = ["Alice", "Bob", "Charlie", "David"]
+attended = ["Charlie", "Eve", "Alice", "Frank"]
+if "Alice" in submitted and "Alice" in attended:
+    print("Alice submitted their assignment and attended class.")
+else:
+    print("Alice did not submit their assignment or did not attend class.")
---
+++ advancedSlicingTechniques.py
@@ -0,0 +1,7 @@
+# Task 1 Given the list of grades:
+grades = [85, 90, 78, 88, 76, 95, 89, 80, 72, 93]
+grades.sort()
+print(grades)
+# Task 2 Calculate the average grade and print it:
+averageGrade = sum(grades) / len(grades)
+print("the average of grades list is", averageGrade)
